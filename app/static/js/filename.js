772cdc0f5c6a720775da88e97cb9d4a1434d999c
Dmitry <twainholm@ya.ru>
2019-09-15 22:24:04 +1000

    small cleanup

--- app/static/js/filename.js
+++ app/static/js/filename.js
@@ -1,30 +1,30 @@
-  function won(wid,did) {
-   /*tselector = "div#p2 p#l2 span#w1"
-   tselector = "div#"+did+" div span#" + wid*/
-   tselector = "#"+did+">div>" + "#"+wid
-   /*console.log("tselector: "+tselector);*/
-   els = document.querySelectorAll(tselector);
-   for ( var i = 0; i < els.length; i++) {
+	function won(wid,did) {
+	/*tselector = "div#p2 p#l2 span#w1"
+	tselector = "div#"+did+" div span#" + wid*/
+	tselector = "#"+did+">div>" + "#"+wid
+	/*console.log("tselector: "+tselector);*/
+	els = document.querySelectorAll(tselector);
+	for ( var i = 0; i < els.length; i++) {
 		/* #BFB63B - green, sort of */
 		/* #FFFF00 - yellow original */
 		/* #F26B6B - fancy red */
 		els[i].style = "background-color: #FFFF00";
 	}
-   /*translation.style="background-color: #FFFF00";*/
-  }
-  function woff(wid,did) {
-   /*tselector = "div#"+did+" div span#" + wid;*/
-   tselector = "#"+did+">div>" + "#"+wid
-   els = document.querySelectorAll(tselector);
-   for ( var i = 0; i < els.length; i++) {
-		els[i].style = "";
+	/*translation.style="background-color: #FFFF00";*/
 	}
-  }
-  window.onload = function () {
-  var rumples = document.getElementsByClassName("word");
-  console.log('we have elts: '+rumples.length);
+	function woff(wid,did) {
+	/*tselector = "div#"+did+" div span#" + wid;*/
+	tselector = "#"+did+">div>" + "#"+wid
+	els = document.querySelectorAll(tselector);
+		for ( var i = 0; i < els.length; i++) {
+			els[i].style = "";
+		}
+	}
+	window.onload = function () {
+	var rumples = document.getElementsByClassName("word");
+	/* console.log('we have elts: '+rumples.length); */
 	for ( var i = 0; i < rumples.length; i++) {
-		console.log('working on element '+i);
+		/* console.log('working on element '+i); */
 		rumples[i].onmouseover = function() { won(this.id,this.parentNode.parentNode.id); };
 		rumples[i].onmouseout = function() { woff(this.id,this.parentNode.parentNode.id); };
 	}
